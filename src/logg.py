cddf58e5d91eece826964ac102de3b8072114c0a
Andreas <249964@usn.no>
2026-02-12 18:33:20 +0100

    Added comments

--- src/logg.py
+++ src/logg.py
@@ -1,7 +1,10 @@
 import logging
+
+# A logger function. Add @defaultLogger above any function to add its output to the log file
+
 logFile = "./logfile.log"
 logLevel = logging.INFO
-logging.basicConfig(level=logLevel, filename=logFile, filemode="a", format="%(asctime)-15s %(levelname)-8s %(message)s")
+logging.basicConfig(level=logLevel, filename=logFile, filemode="a", format="%(asctime)-15s %(levelname)-8s %(message)s") #Formating for the log entries
 
 logger = logging.getLogger("logger")
 import functools
@@ -11,7 +14,7 @@ def logg(func, logger: logging.Logger):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         try:
-            logging.info(f"{func.__name__} | {result}")
+            logging.info(f"{func.__name__} | {result}") # adds the function name and result(output) to the log file
         except Exception as e:
             logging.exception(e)
         return result
