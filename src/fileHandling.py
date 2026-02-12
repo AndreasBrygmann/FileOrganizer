07390b72acb671d48847817022693ea259246534
Andreas <249964@usn.no>
2026-02-12 18:34:26 +0100

    Added comments

--- src/fileHandling.py
+++ src/fileHandling.py
@@ -1,8 +1,9 @@
 import sys
 from csv import reader
 
-fileTypeFile = sys.path[0].rpartition("\\")[0] + "\\file_types.csv"
+fileTypeFile = sys.path[0].rpartition("\\")[0] + "\\file_types.csv" #Looks for the file "file_types.csv" in the "fileorganizer" folder
 
+#Reads filenames from the filetypes file
 def readFileTypes():
     fileTypes = []
     try:
