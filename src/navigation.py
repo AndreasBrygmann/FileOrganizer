063603c12053677d307c1f1e2fd11d73d4aa64a8
AndreasBrygmann <jonab89@msn.com>
2026-02-13 15:03:01 +0100

    checks if directory exists before navigating to it

--- src/navigation.py
+++ src/navigation.py
@@ -14,14 +14,18 @@ def listFiles(path): #Lists the contents of your current directory
         return None 
 
 def defaultDir(): #Returnes the system path
-    #path = sys.path[0] #Sets the path to the program folder
-    path = os.path.expanduser("~") #Sets the path to the users home folder
+    #path = sys.path[0] #Sets the default path to the program folder
+    path = os.path.expanduser("~") #Sets the default path to the users home folder
     return path
 
 def navigateForward(path,folder): #returns the path to a sub directory of the current path
     folder = folder[2:].replace(" ", "")
-    path += f'\\{folder}'
-    return path
+    newPath = path + f'\\{folder}'
+    if os.path.isdir(newPath): #Checks that the folder exists
+        return newPath
+    else:
+        print(f'Could not find directory "{path}"')
+        return path
 
 def navigateBackward(path): #returns the path to the parent directory of the current path
     return path.rpartition(f'\\')[0]
